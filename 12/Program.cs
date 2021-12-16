28174718758d87ec5403183b7ef853ae4ad41fa5
Johan Lindqvist <joli80@gmail.com>
2021-12-16 23:53:22 +0100

    add puzzle input

--- 12/Program.cs
+++ 12/Program.cs
@@ -14,6 +14,32 @@ kj-sa
 kj-HN
 kj-dc";
 
+string secondSystem = @"BC-gt
+gt-zf
+end-KH
+end-BC
+so-NL
+so-ly
+start-BC
+NL-zf
+end-LK
+LK-so
+ly-KH
+NL-bt
+gt-NL
+start-zf
+so-zf
+ly-BC
+BC-zf
+zf-ly
+ly-NL
+ly-LK
+IA-bt
+bt-so
+ui-KH
+gt-start
+KH-so";
+
 SystemBuilder systemBuilder = new();
 var caves = systemBuilder.Build(firstSystem);
 
@@ -27,6 +53,32 @@ var paths = new PathFinder().Find(caves.First());
 Console.WriteLine($"*** {paths.Count} PATHS ***");
 
 foreach (var path in paths)
+{
+    Console.WriteLine($"{string.Join(",", path)}");
+}
+
+Console.WriteLine("*************************************");
+
+caves = systemBuilder.Build(secondSystem);
+
+foreach (var cave in caves)
+{
+    Console.WriteLine($"{cave} ({(cave.IsSmall ? "small" : "large")}): {string.Join(",", cave.Caves)}");
+}
+
+paths = new PathFinder().Find(caves.First());
+Console.WriteLine($"*** {paths.Count} PATHS ***");
+
+// foreach (var path in paths)
+// {
+//     Console.WriteLine($"{string.Join(",", path)}");
+// }
+
+var oneSmallCave = paths.Where(p => p.Count(c => c.IsSmall) < 2).ToList();
+
+Console.WriteLine($"*** At most one small cave: {oneSmallCave.Count} PATHS ***");
+
+foreach (var path in oneSmallCave)
 {
     Console.WriteLine($"{string.Join(",", path)}");
 }
